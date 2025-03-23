11e378e97924c5422f7b9a05ebc9a1f2e97d078c
mikaeltorni <mikaeltorni25@gmail.com>
2025-03-23 18:15:20 +0200

    Fix the test, client and server connection working now

--- cpptest.py
+++ cpptest.py
@@ -1,10 +1,9 @@
-
 import requests
 import json
 
 payload = {
     "prompt": "Write a very long poem about a cat",
-    "max_tokens": 4000
+    "n_predict": 4000
 }
-response = requests.post("http://localhost:8080/generate", json=payload)
+response = requests.post("http://localhost:8080/completion", json=payload)
 print(json.dumps(response.json(), indent=2))
